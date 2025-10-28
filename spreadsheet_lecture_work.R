19aed48b9edfdd3a992bc2324ce2f7656e160392
Will McL <william.mcloughlin@stud-mail.uni-wuerzburg.de>
2025-10-28 11:10:41 +0100

    10/28 spreadsheet intro

--- spreadsheet_lecture_work.R
+++ spreadsheet_lecture_work.R
@@ -1,2 +1,34 @@
 ?read.csv()
-read.csv("Month_spreadsheet.csv")
+months.df <- read.csv("Month_spreadsheet.csv")
+summary(months.df)
+head(months.df)
+tail(months.df)
+
+#install.packages("datapasta")
+library(datapasta)
+??datapasta
+tribble_paste(months.df)
+tibble::tribble(
+  ~Number,       ~Name,
+       1L,   "January",
+       2L,  "February",
+       3L,     "March",
+       4L,     "April",
+       5L,       "May",
+       6L,      "June",
+       7L,      "July",
+       8L,    "August",
+       9L, "September",
+      10L,   "October",
+      11L,  "November",
+      12L,  "December"
+  )
+df_paste(months.df)
+data.frame(
+  stringsAsFactors = FALSE,
+            Number = c(1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L, 11L, 12L),
+              Name = c("January","February","March",
+                       "April","May","June","July","August","September",
+                       "October","November","December")
+)
+
